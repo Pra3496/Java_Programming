ab68871e5f7428f9644329e0d4a07aee92d9516a
Pranav Waghmare <79601856+Pra3496@users.noreply.github.com>
2023-01-22 01:17:08 +0530

    Add files via upload

--- DemoPrint.java
+++ DemoPrint.java
@@ -1,7 +1,8 @@
-Class Demo
+
+class Program193
 {
-  public static void main(String []args)
-  {
-    System.out.println("Jay Ganesh");
-  }
+	public static void main(String arg[])
+	{
+		System.out.println("Jay Ganesh");
+	}
 }
